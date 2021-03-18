2e4376433ddd676ad9c3461a124ea39933b846a6
MB-study <MB-study@192.168.1.142>
2021-03-18 20:39:38 +0800

    Java

--- src/main/java/com/ocp/day2/BMICase1.java
+++ src/main/java/com/ocp/day2/BMICase1.java
@@ -1,41 +1,62 @@
 package com.ocp.day2;
+
 import java.util.*;
-class bmi{
+import java.io.*;
+
+class bmi {
+
     String name;
-    double height,weight;
+    double height, weight;
     Scanner sc = new Scanner(System.in);
-    public bmi(String name,double h,double w) {
-        this.name = name;
-        this.height = h;
-        this.weight = w; 
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public bmi() {
+
     }
 
-    public double getbmi(){
-        double bmi = weight / Math.pow(height/100, 2);
+    public double getbmi() {
+        double bmi = weight / Math.pow(height / 100, 2);
         return bmi;
     }
-    String getResultValue(){
+
+    String getResultValue() {
         double bmiValue = getbmi();
-        String resuleValue = (bmiValue>23)?"過重":(bmiValue<18)?"過輕":"正常";
+        String resuleValue = (bmiValue > 23) ? "過重" : (bmiValue < 18) ? "過輕" : "正常";
         return resuleValue;
+
     }
-    void print(){
+
+    void print() throws IOException {
+        System.out.print("請輸入名字:");
+        this.name = br.readLine();
+
+        if (!this.name.matches("0-9")) {
+            System.out.println("姓名輸入錯誤，請再輸入一次");
+            return;
+        }else{
+            
+        }
+
+        System.out.print("請輸入身高:");
+        this.height = sc.nextDouble();
+        System.out.print("請輸入體重:");
+        this.weight = sc.nextDouble();
         double bmiValue = getbmi();
-        String resuleValue = (bmiValue>23)?"過重":(bmiValue<18)?"過輕":"正常";
+        String resuleValue = (bmiValue > 23) ? "過重" : (bmiValue < 18) ? "過輕" : "正常";
         System.out.printf("姓名:%s 身高:%.1f 體重:%.1f bmi:%.2f 結果:%s\n",
-                name,height,weight,getbmi(),getResultValue());
+                name, height, weight, getbmi(), getResultValue());
+
     }
 }
 
 public class BMICase1 {
 
-    public static void main(String[] args) {
-        
-        bmi h1 = new bmi("Ming",170,60);
+    public static void main(String[] args) throws IOException {
+
+        bmi h1 = new bmi();
         h1.print();
-        bmi h2 = new bmi("Ting",150,60);
+        bmi h2 = new bmi();
         h2.print();
-        
-       
-    } 
+
+    }
 }
