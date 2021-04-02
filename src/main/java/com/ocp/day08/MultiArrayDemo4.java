11501cae27bb92fa435dec8010eef1c64c6fce76
偉綸 <80758652+erick21033@users.noreply.github.com>
2021-04-03 02:01:24 +0800

    Update MultiArrayDemo4.java

--- src/main/java/com/ocp/day08/MultiArrayDemo4.java
+++ src/main/java/com/ocp/day08/MultiArrayDemo4.java
@@ -5,11 +5,12 @@ public class MultiArrayDemo4 {
     public static void main(String[] args) {
         double[][]hw = {{170,160,180},{60,45,90}};
         
-        for(int i=0;i<hw.length;i++){
-            for(int k=0;k<hw[1].length;k++){
-                double bmi = 0;
-            }
-        }
+    for (int i = 0; i < 1; i++) {
+			for (int k = 0; k < hw[1].length; k++) {
+				double bmi = hw[i + 1][k] / Math.pow(hw[i][k] / 100, 2);
+				System.out.printf("bmi: %.2f\n", bmi);
+			}
+		}
         
     }
 }
