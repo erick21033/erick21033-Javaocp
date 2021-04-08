1072d7db3e4986e9822670f9b1d434af5801ccdc
MB-study <MB-study@192.168.1.142>
2021-04-08 19:11:45 +0800

    Java

---
+++ src/main/java/com/ocp/day10/PersonMain.java
@@ -0,0 +1,14 @@
+
+package com.ocp.day10;
+
+
+public class PersonMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
