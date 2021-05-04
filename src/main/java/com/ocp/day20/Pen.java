0c7949d52f110497ac1c4b375e020f7d12f24abf
MB-study <MB-study@192.168.1.142>
2021-05-04 19:51:00 +0800

    Java

---
+++ src/main/java/com/ocp/day20/Pen.java
@@ -0,0 +1,32 @@
+package com.ocp.day20;
+
+public class Pen {
+    private String color;
+    private int price;
+
+    public Pen(String color, int price) {
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        return 7; 
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Pen p  = (Pen)obj;
+        if(price == p.price&&color.equals(p.color)){
+            return true;
+        }
+        return false; 
+    }
+    
+    @Override
+    public String toString() {
+        return "Pen{" + "color=" + color + ", price=" + price + '}';
+    }
+    
+    
+}
---
+++ src/main/java/com/ocp/day20/PenEqualsDemo.java
@@ -0,0 +1,16 @@
+package com.ocp.day20;
+
+public class PenEqualsDemo {
+    public static void main(String[] args) {
+        Pen p1 = new Pen("red",10);
+        Pen p2 = new Pen("red",10);
+        System.out.println(p1);
+        System.out.println(p2);
+        //System.out.println(p1 == p2);
+        System.out.println(p1.equals(p2));
+        
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
+    }
+  
+}
