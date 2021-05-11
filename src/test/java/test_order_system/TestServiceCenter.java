f039640fe103f1b8d520b92e62a1f8d92fbbacbf
MB-study <MB-study@192.168.1.142>
2021-05-11 20:55:25 +0800

    Java

---
+++ src/test/java/test_order_system/TestServiceCenter.java
@@ -0,0 +1,14 @@
+package test_order_system;
+
+import com.lab.order_system.ServiceCenter;
+import org.junit.Test;
+
+public class TestServiceCenter {
+    @Test
+    public void test_printCatalog(){
+        ServiceCenter sc = new ServiceCenter();
+        sc.printCatalog();
+    }
+    
+}
+
