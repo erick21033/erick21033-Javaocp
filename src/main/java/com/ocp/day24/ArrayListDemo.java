971a80fb4840f652cc3525cac507e6be390aa0d4
MB-study <MB-study@192.168.1.142>
2021-05-11 19:28:25 +0800

    Java

---
+++ src/main/java/com/ocp/day24/ArrayListDemo.java
@@ -0,0 +1,28 @@
+package com.ocp.day24;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<String>list = new ArrayList<>();
+        list.add("A");
+        list.add("C");
+        list.add(0,"B");
+        list.add("C");
+        System.out.println(list);
+        //走訪器
+        ListIterator<String>iter = list.listIterator();
+        while (iter.hasNext()) {
+            int nextID = iter.nextIndex();
+            String next = iter.next();
+            System.out.printf("%d : %s\n",nextID,next);
+        }
+           while (iter.hasPrevious()) {
+            int PrevID = iter.nextIndex();
+            String Prev = iter.previous();
+            System.out.printf("%d : %s\n",PrevID,Prev);
+        }
+    }
+}
