5f893b44b0d2a81868c8f293ad339a094f6a8cfa
MB-study <MB-study@192.168.1.142>
2021-05-13 19:29:22 +0800

    Java

---
+++ src/main/java/com/lab/cdc/Util.java
@@ -0,0 +1,21 @@
+
+package com.lab.cdc;
+//工具程式
+
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Util {
+    public static String getJson()throws Exception{
+        // 資料來源
+        String path = "https://www.cdc.gov.tw/TravelEpidemic/ExportJSON";
+        // 建立 URL 物件
+        URL url = new URL(path);
+        // 取得 json 文字資料 ("\\A" 全部選擇)
+        String json = new Scanner(new InputStreamReader(url.openStream(), "UTF-8"))
+                .useDelimiter("\\A")// \\A全選
+                .next();
+        return (json.charAt(0)=='?')?json.substring(1):json;
+    }
+}
