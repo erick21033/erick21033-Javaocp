3793fd2f80bca280b171dd3db4835e2e279663b9
MB-study <MB-study@192.168.1.142>
2021-05-13 21:24:06 +0800

    Java

---
+++ src/main/java/com/lab/rice/Main.java
@@ -0,0 +1,18 @@
+package com.lab.rice;
+
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Stream.of(Util.getRices())
+                .filter(r -> r.get品名().contains("日本"))
+                .forEach(System.out::println);
+        //將指定不合格的米放入集合中
+        List badRices = Stream.of(Util.getRices())
+                .filter(r->r.get品名().contains("日本"))
+                .collect(toList());
+        System.out.printf("不合格筆數: %d,內容: %s\n",badRices.size(),badRices);
+    }
+}
---
+++ src/main/java/com/lab/rice/Util.java
@@ -0,0 +1,25 @@
+
+package com.lab.rice;
+
+import com.google.gson.Gson;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Util {
+    public static String getJson()throws Exception{
+        String path = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
+        URL url = new URL(path);
+        String json = new Scanner(new InputStreamReader(url.openStream(),"UTF-8"))
+                .useDelimiter("\\A")// \\A全選
+                .next();
+        return json;
+    }
+    
+    public static Rice[] getRices() throws Exception {
+        String json = getJson();
+        Rice[] rices = new Gson().fromJson(json, Rice[].class);
+        return rices;
+    }
+}
